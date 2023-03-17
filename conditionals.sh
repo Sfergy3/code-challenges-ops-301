ff6cceac1422b26f71ed5d53ad2c9015ed5e9979
s3 <stanleyl.fergusoniii@gmail.com>
2023-03-17 02:17:05 +0000

    code incomplete working out syntax errors

---
+++ conditionals.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#conditions
+#Stanley L. Ferguson III
+#16 mar 2023
+#give user a prompt of options
+
+echo "what would you like to do today? enter:
+1) print hello world
+2) ping self
+3) ip info
+4) exit"
+read input
+
+if [[ $input -eq 1 ]]; then
+    echo "hello world"
+
+elif [[$input -eq 2]]; then
+    echo "hit Ctl C to stop"
+    ping 192.168.1.204
+
+elif [[$input -eq 3]]; then
+     hostname -I
+     
+elif [[$input -eq 4]]; then
+     exit 
+fi
