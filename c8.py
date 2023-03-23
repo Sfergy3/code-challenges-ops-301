5b224c0015b872ae0da401b9556a4664fef8b45f
s3 <stanleyl.fergusoniii@gmail.com>
2023-03-23 00:56:14 +0000

    HAIL TO THE REDSKINS

---
+++ c8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# challenge 8 
+# Stanley L. Ferguson III
+# 22 Mar 2023
+# create and print elements of a list in python.
+
+# create an array
+
+httr = ["left", "hand", "up", "who", "are", "we", "the", "god", "damn", "commanders"]
+
+# print the whole list
+print(httr)
+
+# print the fourth item
+print(httr[3])
+
+# print item range 6-10
+print(httr[5:10])
+
+# replace item 7 
+httr[6] = "flippin"
+print(httr)
+
+# end
