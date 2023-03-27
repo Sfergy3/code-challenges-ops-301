614a1dde172ffcef621fb5ac1c7ddb413118a6a1
Stanley  L. Ferguson III <123392950+Sfergy3@users.noreply.github.com>
2023-03-27 10:03:46 -0500

    Update ch9.py

--- ch9.py
+++ ch9.py
@@ -1,1 +1,51 @@
-Gituation, push keeps timing out
+# get a and b from user input
+a = int(input("Enter a value: "))
+b = int(input("Enter b value: "))
+
+# a equals b
+if a == b:
+    print("a is equal to b")
+
+# a not equal to b
+if a != b:
+    print("a is not equal to b")
+
+# a less than b
+if a < b:
+    print("a is less than b")
+
+# a less than or equal to b
+if a <= b:
+    print("a is less than or equal to b")
+
+# a greater than b
+if a > b:
+    print("a is greater than b")
+
+# a greater than or equal to b
+if a >= b:
+    print("a is greater than or equal to b")
+
+# create if statements with logical conditions
+if a > b:
+    print("a is greater than b")
+elif a == b:
+    print("a is equal to b")
+else:
+    print("a is less than b")
+
+# use elif and else to meet different conditions
+if a > b:
+    print("a is greater than b")
+elif a == b:
+    print("a is equal to b")
+else:
+    print("a is less than b")
+    if b - a >= 8:
+        print("The difference between a and b is greater than or equal to 8")
+    elif b - a >= 41:
+        print("The difference between a and b is greater than or equal to 41")
+    else:
+        print("The difference between a and b is less than 8")
+
+    # End Code
